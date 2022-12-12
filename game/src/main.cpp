abb9ccb371f3b00f9c3798cc9038b7d24cd3bcfc
QwEekYhyo <qweekyhyo.yt83@gmail.com>
2022-12-12 22:41:50 +0100

    yep, this is big brain time

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,21 +1,14 @@
 #include <board.hpp>
-#include <string>
-#include <sstream>
 
 int main(void) {
     board b;
-    std::stringstream ss;
-    std::string line;
     int c;
 
     while (true) {
         std::cout << b << '\n';
         std::cout << "Column : ";
-        std::getline(std::cin, line);
-        ss << line;
-        ss >> c;
+        std::cin >> c;
         b.play(c);
-        ss.clear();
         std::cout << '\n';
     }
     return 0;
