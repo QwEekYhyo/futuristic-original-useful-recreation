536f56a419c1adf5ec5bc358a261500a856659f2
QwEekYhyo <qweekyhyo.yt83@gmail.com>
2023-01-13 14:36:42 +0100

    Revert "for romain"
    This reverts commit 9eb31c0b5e9c3bba196e9a1af258796e93a1421a.

--- game/src/utils.cpp
+++ game/src/utils.cpp
@@ -10,7 +10,8 @@ void reassign(int *arr1, int *arr2) {
     if (len1 != len2) {
         return;
     }
-    for (int i = 0; i <= len1; i++) {
+    for (int i = 0; i < len1; i++) {
+        std::cout << arr1[i] << " and " << arr2[i] << '\n';
         arr1[i] = arr2[i];
     }
 }
